893a3f25d2a16332343d9e54169db34d56b0608a
qolkrex <qolkrex@gmail.com>
2025-07-01 16:25:31 -0500

    corrigiendo el modla del ticket

--- src/components/ModalForm.tsx
+++ src/components/ModalForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
-import Image from "next/image";
+//import Image from "next/image";
 import axios from "axios";
 import "@/styles/modal.css";
 
